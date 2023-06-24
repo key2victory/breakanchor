fa44ebaddabb0bc6e419f872fec3931f89e6a7bf
Janna Curtis <curtisjm@ornl.gov>
2023-06-24 02:48:27 -0400

    update iframe styles for PageSample component

--- src/PageSamples.js
+++ src/PageSamples.js
@@ -46,7 +46,15 @@ function PageSample({ index }) {
         title="Sample"
         name="mockframe"
         className=""
-        style={{ position: "absolute", top: 0, right: 0, bottom: 0, left: 0 }}
+        style={{
+          gridRow: "content / span 1",
+          width: "100%",
+          height: "100%",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+        }}
         src={samplePages[index].src}
       />{" "}
     </div>
