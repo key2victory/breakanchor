75c0e3e8927ca4846a6dd87bf583da9f0c4b113f
Janna Curtis <curtisjm@ornl.gov>
2023-06-26 13:03:41 -0400

    improvements to PageIntuitive, added ButtonLink to slide deck

--- src/PageIntuitive.js
+++ src/PageIntuitive.js
@@ -1,6 +1,6 @@
-import { memo } from "react";
-import { HeroIntuitive } from "./HeroIntuitive";
-import { Header, ButtonLink, Note, Group } from "./PageElements";
+import {memo} from "react";
+import {HeroIntuitive} from "./HeroIntuitive";
+import {Header, ButtonLink, Note, Group} from "./PageElements";
 
 export const PageIntuitive = memo(function PageIntuitive({
   title,
@@ -18,23 +18,31 @@ export const PageIntuitive = memo(function PageIntuitive({
       hero: "iframe",
       background:
         "linear-gradient(90deg, hsl(256, 30%, 60%) 0%, hsl(228, 40%, 70%) 40%)",
-      background1: "hsl(228, 40%, 70%)",
-      background2: "hsl(218,77%,88%)",
+      background1: "hsl(228, 40%, 70%)", //"hsl(258, 34%, 87%)", //"hsl(256, 60%, 29%)",
+      background2: "hsl(218,77%,88%)", //"hsl(228,55%,32%)",
     },
   ];
   const cardHeader = {
+    // background: "hsl(240, 18%, 58%)",
+    // background: "hsl(240, 80%, 95%)",
     background: "hsla(240, 80%, 90%, 10%)",
     color: "hsl(240,30%,50%)",
     padding: "1rem 1rem",
     borderRadius: ".5rem",
   };
   const cardSection = {
+    // borderLeft: "10px solid hsl(240,50%,60%)",
     borderRadius: "1rem",
     padding: "1rem",
     background: "hsla(240,20%,100%,100%)",
   };
-  const color1 = "hsl(240, 60%, 80%)";
-  const color2 = "hsl(240, 80%, 85%)";
+  //const color1 = "hsl(0,0%,80%)";
+  //const color2 = "hsl(0,0%,90%)";
+  const color1 = "hsl(245, 60%, 62%)"; //"hsl(240, 80%, 92%)";
+  const color2 = "transparent"; //"hsl(235, 60%, 64%)"; //"hsl(240, 80%, 95%)";
+  const color3 = "hsl(235, 60%, 64%)";
+  const color4 = "hsl(235, 40%, 62%)";
+
   return (
     <div className="main-content">
       <div
@@ -43,21 +51,48 @@ export const PageIntuitive = memo(function PageIntuitive({
           background: `linear-gradient(45deg, ${color1} 25%, ${color2} 0, ${color2} 50%, ${color1} 0, ${color1} 75%, ${color2} 0) center / 30px 30px`,
         }}
       />{" "}
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+
+          zIndex: "-11",
+          background: `linear-gradient(${color3} 50%,${color4} 80%)`,
+        }}
+      />{" "}
       <div className="scroll-zone">
+        {" "}
+        {/* <div
+                      className="page-bg"
+                      style={{
+                        background:
+                          "linear-gradient(90deg, hsl(256, 30%, 60%) 0%, hsl(228, 40%, 70%) 40%)"
+                      }}
+                    />*/}{" "}
         <Header
           title={title}
           title1="What Makes"
           title2="UI Intuitive?"
           subtitle="Codestock Speaker Session"
-          color="hsl(0,0%,0%)"
-        />
+          color="hsl(0,0%,100%)"
+        >
+          <ButtonLink
+            color="white"
+            line1="see the full"
+            line2="slide deck"
+            url="https://docs.google.com/presentation/d/1sX_i-tTYVkihPYqb25-01_tm7hDQsf-zdyhIPcvHed8/edit?usp=sharing"
+          />
+        </Header>{" "}
         <Group className="desktop-row tablet-row mobile-col">
           <Note
             title="Synopsis"
             description="Computers aren’t natural, yet people use the word “intuitive” to describe great software – It’s not a mistake. In this session, we'll examine how design interfaces with the human brain."
             background="hsl(240, 80%, 90%)"
             collapse={false}
-            style={{ maxWidth: "500px" }}
+            style={{maxWidth: "500px"}}
           />{" "}
         </Group>{" "}
         <Group
@@ -66,7 +101,7 @@ export const PageIntuitive = memo(function PageIntuitive({
             ...cardSection,
           }}
         >
-          <Group className="col" style={{ ...cardHeader }}>
+          <Group className="col" style={{...cardHeader}}>
             <h2> 1. Cognitive Load </h2>{" "}
           </Group>{" "}
           <Group>
@@ -87,7 +122,7 @@ export const PageIntuitive = memo(function PageIntuitive({
             ...cardSection,
           }}
         >
-          <Group className="col" style={{ ...cardHeader }}>
+          <Group className="col" style={{...cardHeader}}>
             <h2> 2. Implicit Memory </h2>{" "}
             <span>
               Conceptual model from psychology used to describe how human
@@ -106,25 +141,25 @@ export const PageIntuitive = memo(function PageIntuitive({
             />
           </Group>{" "}
         </Group>{" "}
-        <Group className="card col" style={{ ...cardSection }}>
-          <Group style={{ ...cardHeader }}>
+        <Group className="card col" style={{...cardSection}}>
+          <Group style={{...cardHeader}}>
             <h2> 3. Semiotics </h2>{" "}
           </Group>{" "}
           <Group>
             <img className="image" src="./img/intuitive-12.png" alt="" />
           </Group>{" "}
         </Group>{" "}
-        <Group className="card col" style={{ ...cardSection }}>
-          <Group className="col" style={{ ...cardHeader }}>
+        <Group className="card col" style={{...cardSection}}>
+          <Group className="col" style={{...cardHeader}}>
             <h2> Session Feedback </h2>{" "}
             <h3>
               {" "}
               What part was most memorable ? Did you learn something new ?{" "}
             </h3>{" "}
-          </Group>{" "}
+          </Group>
           <Group
             className="desktop-row tablet-row mobile-col"
-            style={{ padding: "0 1rem 1rem 1rem" }}
+            style={{padding: "0 1rem 1rem 1rem"}}
           >
             <span
               style={{
