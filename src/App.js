b8647dd8c03679ce72aff7ca2109af273edde3b7
Janna Curtis <curtisjm@ornl.gov>
2023-06-28 22:45:43 -0400

    added new device sizing style sheets

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
 import {useState} from "react";
-import AppNav, {AppHeader} from "./AppNav";
-
+import AppNav, {AppHeader, NavPanel} from "./AppNav";
 import {Outlet} from "react-router-dom";
 import "./styles.css";
+import "./styles_utility.css";
 import "./styles_mobile.css";
+import "./styles_phablet.css";
 import "./styles_tablet.css";
+import "./styles_laptop.css";
 import "./styles_desktop.css";
 
 export default function App(props) {
   const [showNav, setShowNav] = useState(false);
 
   return (
-    <div
-      className="App"
-      style={
-        {
-          //background: "hsl(0,0%,100%)"
-          //  "linear-gradient(0deg, hsl(0,0%,100%) 30%, hsl(0,0%,50%) 30%)"
-        }
-      }
-    >
+    <div className="App">
       <AppHeader
         onMenuClick={() => {
           setShowNav(true);
         }}
       />{" "}
+      <NavPanel />
       <AppNav
         showNav={showNav}
         onClickExit={() => {
           setShowNav(false);
         }}
-      />
-      <Outlet />
+      />{" "}
+      <Outlet
+        style={{gridColumn: "content / span 1", gridRow: "content / span 1"}}
+      />{" "}
     </div>
   );
 }
