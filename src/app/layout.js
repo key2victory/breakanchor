3ec068e4256038bf0c6c583a2e69b353391b1a41
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-10-05 23:15:36 -0400

    Nukes repo and starts fresh with Next 13 and Tailwind

---
+++ src/app/layout.js
@@ -0,0 +1,31 @@
+import 'src/styles/globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Break Anchor Design',
+  description: "A porfolio site to showcase Janna Curtis' work in design",
+  applicationName: 'Break Anchor',
+  authors: {
+    name: 'Tyler Simoni',
+    url: 'https://tylersimoni.com',
+  },
+  keywords: [
+    'janna curtis',
+    'design',
+    'research',
+    'portfolio',
+    'next13',
+    'tailwindcss',
+    'framer-motion',
+  ],
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
