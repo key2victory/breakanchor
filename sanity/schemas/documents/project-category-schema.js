4d22ccf7dd80da618ca18823248c49225b1583d4
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-11-01 17:22:00 -0400

    Cleans up project schema and gets initial color for cards working

--- sanity/schemas/documents/project-category-schema.js
+++ sanity/schemas/documents/project-category-schema.js
@@ -1,33 +1,26 @@
-import { defineType, defineField, defineArrayMember } from 'sanity';
-import colors from 'tailwindcss/colors';
+import { defineType, defineField } from 'sanity';
 
-const colorList = {
-  red: colors.red,
-  orange: colors.orange,
-  amber: colors.amber,
-  yellow: colors.yellow,
-  lime: colors.lime,
-  green: colors.green,
-  emerald: colors.emerald,
-  teal: colors.teal,
-  cyan: colors.cyan,
-  sky: colors.sky,
-  blue: colors.blue,
-  indigo: colors.indigo,
-  violet: colors.violet,
-  purple: colors.purple,
-  fuchsia: colors.fuchsia,
-  pink: colors.pink,
-  rose: colors.rose,
-};
+// import colors from 'tailwindcss/colors';
 
-const colorPalettes = Object.entries(colorList).map(([key, _]) => ({
-  [key]: {
-    primaryBg: `bg-${key}-500`,
-    hoverBg: `bg-${key}-500`,
-    primaryText: `text-${key}-500`,
-  },
-}));
+// const colorList = {
+//   red: colors.red,
+//   orange: colors.orange,
+//   amber: colors.amber,
+//   yellow: colors.yellow,
+//   lime: colors.lime,
+//   green: colors.green,
+//   emerald: colors.emerald,
+//   teal: colors.teal,
+//   cyan: colors.cyan,
+//   sky: colors.sky,
+//   blue: colors.blue,
+//   indigo: colors.indigo,
+//   violet: colors.violet,
+//   purple: colors.purple,
+//   fuchsia: colors.fuchsia,
+//   pink: colors.pink,
+//   rose: colors.rose,
+// };
 
 const projectCategory = defineType({
   name: 'projectCategory',
@@ -76,40 +69,7 @@ const projectCategory = defineType({
           'rose',
         ],
       },
-      //   Object.entries(value).map(([key, value]) =>
-      //   defineField({ name: key, title: value, type: 'string' }),
-      // ),
-      // list: Object.entries(colors).map(([key, value]) => key)
     }),
-    // defineField({
-    //   name: 'colorTheme',
-    //   title: 'Color Theme',
-    //   type: 'color',
-    //   readOnly: ({ currentUser }) =>
-    //     !currentUser.roles.find(({ name }) => name === 'administrator'),
-    //   options: {
-    //     disableAlpha: true,
-    //     colorList: [
-    //       '#ef4444',
-    //       '#f97316',
-    //       '#f59e0b',
-    //       '#eab308',
-    //       '#84cc16',
-    //       '#22c55e',
-    //       '#10b981',
-    //       '#14b8a6',
-    //       '#06b6d4',
-    //       '#0ea5e9',
-    //       '#3b82f6',
-    //       '#6366f1',
-    //       '#8b5cf6',
-    //       '#a855f7',
-    //       '#d946ef',
-    //       '#ec4899',
-    //       '#f43f5e',
-    //     ],
-    //   },
-    // }),
   ],
 });
 
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,24 +2,86 @@
 
 import { motion } from 'framer-motion';
 import MotionLink from './MotionLink';
-import { useEffect, useRef } from 'react';
+
+const colorThemes = {
+  red: {
+    primaryBg: 'bg-red-500',
+    secondaryBg: 'bg-red-300',
+  },
+  orange: {
+    primaryBg: 'bg-orange-500',
+    secondaryBg: 'bg-orange-300',
+  },
+  amber: {
+    primaryBg: 'bg-amber-500',
+    secondaryBg: 'bg-amber-300',
+  },
+  yellow: {
+    primaryBg: 'bg-yellow-500',
+    secondaryBg: 'bg-yellow-300',
+  },
+  lime: {
+    primaryBg: 'bg-lime-500',
+    secondaryBg: 'bg-lime-300',
+  },
+  green: {
+    primaryBg: 'bg-green-500',
+    secondaryBg: 'bg-green-300',
+  },
+  emerald: {
+    primaryBg: 'bg-emerald-500',
+    secondaryBg: 'bg-emerald-300',
+  },
+  teal: {
+    primaryBg: 'bg-teal-500',
+    secondaryBg: 'bg-teal-300',
+  },
+  cyan: {
+    primaryBg: 'bg-cyan-500',
+    secondaryBg: 'bg-cyan-300',
+  },
+  sky: {
+    primaryBg: 'bg-sky-500',
+    secondaryBg: 'bg-sky-300',
+  },
+  blue: {
+    primaryBg: 'bg-blue-500',
+    secondaryBg: 'bg-blue-300',
+  },
+  indigo: {
+    primaryBg: 'bg-indigo-500',
+    secondaryBg: 'bg-indigo-300',
+  },
+  violet: {
+    primaryBg: 'bg-violet-500',
+    secondaryBg: 'bg-violet-300',
+  },
+  purple: {
+    primaryBg: 'bg-purple-500',
+    secondaryBg: 'bg-purple-300',
+  },
+  fuchsia: {
+    primaryBg: 'bg-fuchsia-500',
+    secondaryBg: 'bg-fuchsia-300',
+  },
+  pink: {
+    primaryBg: 'bg-pink-500',
+    secondaryBg: 'bg-pink-300',
+  },
+  rose: {
+    primaryBg: 'bg-rose-500',
+    secondaryBg: 'bg-rose-300',
+  },
+  slate: {
+    primaryBg: 'bg-slate-800',
+    secondaryBg: 'bg-slate-600',
+  },
+};
 
 const ProjectCard = ({ _id, title, slug, category, selectedType }) => {
-  const cardRef = useRef(null);
-
   const projectCardId = `project-card-${_id}`;
 
-  // const primaryColor = `hsl(${category.color.hsl.h}, ${category.color.hsl.s * 100}%, ${
-  //   category.color.hsl.l * 100
-  // }%)`;
-  // const secondaryColor = `hsl(${category.color.hsl.h}, ${category.color.hsl.s * 0.7 * 100}%, ${
-  //   category.color.hsl.l * 100
-  // }%)`;
-
-  // useEffect(() => {
-  //   if (cardRef.current === null) cardRef.current = document.getElementById(projectCardId);
-  //   cardRef.current.style.setProperty('--project-card-primary', 'primaryColor');
-  // });
+  console.log('Card: ', { title: title, color: category.color });
 
   return (
     <>
@@ -36,12 +98,12 @@ const ProjectCard = ({ _id, title, slug, category, selectedType }) => {
               : selectedType === category.value
               ? 'order-1'
               : 'order-2 before:bg-black/40'
-          }`}
+          } ${colorThemes[category.color ?? 'slate'].primaryBg}`}
         >
           <div className="bg-gray-500 px-4 py-2 w-full">
             <h2 className="text-left text-lg truncate">{title}</h2>
             <p className="rounded-full bg-white text-black px-3 py-1 mt-2 w-fit text-xs">
-              {category.title.toUpperCase()}
+              {category?.title?.toUpperCase() ?? 'PROJECT'}
             </p>
           </div>
         </MotionLink>
@@ -56,12 +118,12 @@ const ProjectCard = ({ _id, title, slug, category, selectedType }) => {
               : selectedType === category.value
               ? 'order-1'
               : 'order-2 before:bg-black/40'
-          }`}
+          } ${colorThemes[category.color ?? 'slate'].primaryBg}`}
         >
           <div className="bg-gray-500 px-4 py-2 w-full">
             <h2 className="text-left text-lg truncate">{title}</h2>
             <p className="rounded-full bg-white text-black px-3 py-1 mt-2 w-fit text-xs">
-              {category.title.toUpperCase()}
+              {category?.title?.toUpperCase() ?? 'PROJECT'}
             </p>
           </div>
         </motion.div>
