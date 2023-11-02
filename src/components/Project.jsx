ef1798d270b5f5e5dc52a3bf5356af243c737dfc
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-11-02 16:29:11 -0400

    Adds color theming and external link button to project page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,149 @@
 import { PortableText } from '@portabletext/react';
 import Link from 'next/link';
 import ImageComponent from '@/sanity/components/Image';
+import { motion } from 'framer-motion';
+
+const colorThemes = {
+  red: {
+    primaryBg: 'bg-red-500',
+    secondaryBg: 'bg-red-600',
+    text: 'text-red-500',
+    hoverBg: 'hover:bg-red-700',
+    hoverText: 'hover:text-red-600',
+  },
+  orange: {
+    primaryBg: 'bg-orange-500',
+    secondaryBg: 'bg-orange-600',
+    text: 'text-orange-500',
+    hoverBg: 'hover:bg-orange-700',
+    hoverText: 'hover:text-orange-600',
+  },
+  amber: {
+    primaryBg: 'bg-amber-500',
+    secondaryBg: 'bg-amber-600',
+    text: 'text-amber-500',
+    hoverBg: 'hover:bg-amber-700',
+    hoverText: 'hover:text-amber-600',
+  },
+  yellow: {
+    primaryBg: 'bg-yellow-500',
+    secondaryBg: 'bg-yellow-600',
+    text: 'text-yellow-500',
+    hoverBg: 'hover:bg-yellow-700',
+    hoverText: 'hover:text-yellow-600',
+  },
+  lime: {
+    primaryBg: 'bg-lime-500',
+    secondaryBg: 'bg-lime-600',
+    text: 'text-lime-500',
+    hoverBg: 'hover:bg-lime-700',
+    hoverText: 'hover:text-lime-600',
+  },
+  green: {
+    primaryBg: 'bg-green-500',
+    secondaryBg: 'bg-green-600',
+    text: 'text-green-500',
+    hoverBg: 'hover:bg-green-700',
+    hoverText: 'hover:text-green-600',
+  },
+  emerald: {
+    primaryBg: 'bg-emerald-500',
+    secondaryBg: 'bg-emerald-600',
+    text: 'text-emerald-500',
+    hoverBg: 'hover:bg-emerald-700',
+    hoverText: 'hover:text-emerald-600',
+  },
+  teal: {
+    primaryBg: 'bg-teal-500',
+    secondaryBg: 'bg-teal-600',
+    text: 'text-teal-500',
+    hoverBg: 'hover:bg-teal-700',
+    hoverText: 'hover:text-teal-600',
+  },
+  cyan: {
+    primaryBg: 'bg-cyan-500',
+    secondaryBg: 'bg-cyan-600',
+    text: 'text-cyan-500',
+    hoverBg: 'hover:bg-cyan-700',
+    hoverText: 'hover:text-cyan-600',
+  },
+  sky: {
+    primaryBg: 'bg-sky-500',
+    secondaryBg: 'bg-sky-600',
+    text: 'text-sky-500',
+    hoverBg: 'hover:bg-sky-700',
+    hoverText: 'hover:text-sky-600',
+  },
+  blue: {
+    primaryBg: 'bg-blue-500',
+    secondaryBg: 'bg-blue-600',
+    text: 'text-blue-500',
+    hoverBg: 'hover:bg-blue-700',
+    hoverText: 'hover:text-blue-600',
+  },
+  indigo: {
+    primaryBg: 'bg-indigo-500',
+    secondaryBg: 'bg-indigo-600',
+    text: 'text-indigo-500',
+    hoverBg: 'hover:bg-indigo-700',
+    hoverText: 'hover:text-indigo-600',
+  },
+  violet: {
+    primaryBg: 'bg-violet-500',
+    secondaryBg: 'bg-violet-600',
+    text: 'text-violet-500',
+    hoverBg: 'hover:bg-violet-700',
+    hoverText: 'hover:text-violet-600',
+  },
+  purple: {
+    primaryBg: 'bg-purple-500',
+    secondaryBg: 'bg-purple-600',
+    text: 'text-purple-500',
+    hoverBg: 'hover:bg-purple-700',
+    hoverText: 'hover:text-purple-600',
+  },
+  fuchsia: {
+    primaryBg: 'bg-fuchsia-500',
+    secondaryBg: 'bg-fuchsia-600',
+    text: 'text-fuchsia-500',
+    hoverBg: 'hover:bg-fuchsia-700',
+    hoverText: 'hover:text-fuchsia-600',
+  },
+  pink: {
+    primaryBg: 'bg-pink-500',
+    secondaryBg: 'bg-pink-600',
+    text: 'text-pink-500',
+    hoverBg: 'hover:bg-pink-700',
+    hoverText: 'hover:text-pink-600',
+  },
+  rose: {
+    primaryBg: 'bg-rose-500',
+    secondaryBg: 'bg-rose-600',
+    text: 'text-rose-500',
+    hoverBg: 'hover:bg-rose-700',
+    hoverText: 'hover:text-rose-600',
+  },
+  slate: {
+    primaryBg: 'bg-slate-800',
+    secondaryBg: 'bg-slate-600',
+    text: 'text-slate-100',
+    hoverBg: 'hover:bg-slate-700',
+    hoverText: 'hover:text-white',
+  },
+};
+
+const arrowVariant = {
+  rest: {
+    x: -20,
+    y: 20,
+    opacity: 0,
+  },
+  hover: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+  },
+};
 
 const components = {
   types: {
@@ -14,7 +157,9 @@ const Project = ({ data }) => {
       <div className="flex items-center gap-6">
         <Link
           href={'/'}
-          className="p-4 rounded-lg hover:bg-indigo-400 transition-colors duration-500 ease-in-out h-full flex items-center"
+          className={`p-4 rounded-lg bg-zinc-900 ${
+            colorThemes[data.category.color ?? 'slate'].hoverBg
+          } transition-colors duration-500 ease-in-out h-full flex items-center`}
         >
           <svg viewBox="0 0 24 24" className="fill-none h-8 w-8">
             <path
@@ -23,15 +168,50 @@ const Project = ({ data }) => {
             />
           </svg>
         </Link>
-        <div>
-          <h1 className="text-6xl font-semibold">{data.title}</h1>
-          <p className="text-xl font-semibold italic mt-4">{data.tagLine}</p>
+        <div className="flex justify-between w-full items-center">
+          <div>
+            <h1
+              className={`text-6xl font-semibold ${
+                colorThemes[data.category.color ?? 'slate'].text
+              }`}
+            >
+              {data.title}
+            </h1>
+            <p
+              className={`text-xl font-semibold italic mt-4 ${
+                colorThemes[data.category.color ?? 'slate'].text
+              }`}
+            >
+              {data.tagLine}
+            </p>
+          </div>
+          {data?.externalLink?.enableLink ? (
+            <Link
+              href={data?.externalLink?.linkUrl}
+              target="_blank"
+              className={`px-6 py-3 rounded-lg bg-zinc-900 ${
+                colorThemes[data.category.color ?? 'slate'].hoverBg
+              } transition-colors duration-500 ease-in-out h-full flex items-center`}
+            >
+              <div className="flex items-center gap-4">
+                <span>{data?.externalLink?.linkName}</span>
+                <span>
+                  <svg className="w-3 h-3" viewBox="0 0 13 13" fill="currentColor">
+                    <path d="M8.04998 6.36001L2.39398 12.018L0.97998 10.603L6.63698 4.94701L1.68698 -0.00299072H13V11.31L8.04998 6.36001Z" />
+                  </svg>
+                </span>
+              </div>
+            </Link>
+          ) : null}
         </div>
       </div>
-      <div role="divider" className="h-1 w-full bg-white my-4" />
+      <div
+        role="divider"
+        className={`h-1 w-full my-4 ${colorThemes[data.category.color ?? 'slate'].primaryBg}`}
+      />
 
       <div className="blog-post-content rounded-md p-2 flex-1 overflow-y-auto">
-        <PortableText className="max-w-[800px]" value={data.content} components={components} />
+        <PortableText value={data.content} components={components} />
       </div>
     </>
   );
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,75 +2,76 @@
 
 import { motion } from 'framer-motion';
 import MotionLink from './MotionLink';
+//import { colorThemes } from '../utils/colors';
 
 const colorThemes = {
   red: {
     primaryBg: 'bg-red-500',
-    secondaryBg: 'bg-red-300',
+    secondaryBg: 'bg-red-600',
   },
   orange: {
     primaryBg: 'bg-orange-500',
-    secondaryBg: 'bg-orange-300',
+    secondaryBg: 'bg-orange-600',
   },
   amber: {
     primaryBg: 'bg-amber-500',
-    secondaryBg: 'bg-amber-300',
+    secondaryBg: 'bg-amber-600',
   },
   yellow: {
     primaryBg: 'bg-yellow-500',
-    secondaryBg: 'bg-yellow-300',
+    secondaryBg: 'bg-yellow-600',
   },
   lime: {
     primaryBg: 'bg-lime-500',
-    secondaryBg: 'bg-lime-300',
+    secondaryBg: 'bg-lime-600',
   },
   green: {
     primaryBg: 'bg-green-500',
-    secondaryBg: 'bg-green-300',
+    secondaryBg: 'bg-green-600',
   },
   emerald: {
     primaryBg: 'bg-emerald-500',
-    secondaryBg: 'bg-emerald-300',
+    secondaryBg: 'bg-emerald-600',
   },
   teal: {
     primaryBg: 'bg-teal-500',
-    secondaryBg: 'bg-teal-300',
+    secondaryBg: 'bg-teal-600',
   },
   cyan: {
     primaryBg: 'bg-cyan-500',
-    secondaryBg: 'bg-cyan-300',
+    secondaryBg: 'bg-cyan-600',
   },
   sky: {
     primaryBg: 'bg-sky-500',
-    secondaryBg: 'bg-sky-300',
+    secondaryBg: 'bg-sky-600',
   },
   blue: {
     primaryBg: 'bg-blue-500',
-    secondaryBg: 'bg-blue-300',
+    secondaryBg: 'bg-blue-600',
   },
   indigo: {
     primaryBg: 'bg-indigo-500',
-    secondaryBg: 'bg-indigo-300',
+    secondaryBg: 'bg-indigo-600',
   },
   violet: {
     primaryBg: 'bg-violet-500',
-    secondaryBg: 'bg-violet-300',
+    secondaryBg: 'bg-violet-600',
   },
   purple: {
     primaryBg: 'bg-purple-500',
-    secondaryBg: 'bg-purple-300',
+    secondaryBg: 'bg-purple-600',
   },
   fuchsia: {
     primaryBg: 'bg-fuchsia-500',
-    secondaryBg: 'bg-fuchsia-300',
+    secondaryBg: 'bg-fuchsia-600',
   },
   pink: {
     primaryBg: 'bg-pink-500',
-    secondaryBg: 'bg-pink-300',
+    secondaryBg: 'bg-pink-600',
   },
   rose: {
     primaryBg: 'bg-rose-500',
-    secondaryBg: 'bg-rose-300',
+    secondaryBg: 'bg-rose-600',
   },
   slate: {
     primaryBg: 'bg-slate-800',
@@ -80,8 +81,6 @@ const colorThemes = {
 
 const ProjectCard = ({ _id, title, slug, category, selectedType }) => {
   const projectCardId = `project-card-${_id}`;
-
-  console.log('Card: ', { title: title, color: category.color });
 
   return (
     <>
@@ -100,7 +99,7 @@ const ProjectCard = ({ _id, title, slug, category, selectedType }) => {
               : 'order-2 before:bg-black/40'
           } ${colorThemes[category.color ?? 'slate'].primaryBg}`}
         >
-          <div className="bg-gray-500 px-4 py-2 w-full">
+          <div className={`px-4 py-2 w-full ${colorThemes[category.color ?? 'slate'].secondaryBg}`}>
             <h2 className="text-left text-lg truncate">{title}</h2>
             <p className="rounded-full bg-white text-black px-3 py-1 mt-2 w-fit text-xs">
               {category?.title?.toUpperCase() ?? 'PROJECT'}
@@ -120,7 +119,7 @@ const ProjectCard = ({ _id, title, slug, category, selectedType }) => {
               : 'order-2 before:bg-black/40'
           } ${colorThemes[category.color ?? 'slate'].primaryBg}`}
         >
-          <div className="bg-gray-500 px-4 py-2 w-full">
+          <div className={`px-4 py-2 w-full ${colorThemes[category.color ?? 'slate'].secondaryBg}`}>
             <h2 className="text-left text-lg truncate">{title}</h2>
             <p className="rounded-full bg-white text-black px-3 py-1 mt-2 w-fit text-xs">
               {category?.title?.toUpperCase() ?? 'PROJECT'}
