9849cbae4759500f1647ae1bea598b65fd4a7ee7
Tyler Simoni <tyler.simoni.8@gmail.com>
2023-11-04 09:57:52 -0400

    Auto stash before merge of "tsimmz/next-overhaul" and "origin/key2victory/next-overhaul"

--- sanity/components/ImageGallery.jsx
+++ sanity/components/ImageGallery.jsx
@@ -4,47 +4,47 @@ import Image from 'next/image';
 import { useState } from 'react';
 
 const FilmstripGrid = ({ children }) => {
-   // const { width, height } = getImageDimensions(value);
-    return (
-        <div
-          style={{
-            width: "100%",
-            display: 'flex',
-            flexFlow: "row wrap",
-            justifyContent: "flex-start",
-            alignItems: "flex-start",
-            gap: ".5rem"
-          }}
-        >
-       {children}
-        </div>
-    );
-  };
+  // const { width, height } = getImageDimensions(value);
+  return (
+    <div
+      style={{
+        width: '100%',
+        display: 'flex',
+        flexFlow: 'row wrap',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        gap: '.5rem',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
-  const LightboxModal = ({ children, showModal}) => {
-     return (
-         <div
-           style={{
-             display: showModal ? 'flex' : 'none',
-             flexFlow: "column nowrap",
-             justifyContent: "center",
-             alignItems: "center",
-             position: 'fixed',
-             top: showModal ? 0 : -1,
-             left: showModal ? 0 : -1,
-             width: showModal ? '100%' : '1',
-             height: showModal ? '100%' : '1',
-             padding: showModal ? '2rem' : 0,
-             background: 'hsla(0,0%,0%,.8)',
-             overflow: 'hidden',
-             zIndex: 1000,
-             backdropFilter: "blur(10px)",
-           }}
-         >
-          
-{children}
-         </div>
-         )}
+const LightboxModal = ({ children, showModal }) => {
+  return (
+    <div
+      style={{
+        display: showModal ? 'flex' : 'none',
+        flexFlow: 'column nowrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'fixed',
+        top: showModal ? 0 : -1,
+        left: showModal ? 0 : -1,
+        width: showModal ? '100%' : '1',
+        height: showModal ? '100%' : '1',
+        padding: showModal ? '2rem' : 0,
+        background: 'hsla(0,0%,0%,.8)',
+        overflow: 'hidden',
+        zIndex: 1000,
+        backdropFilter: 'blur(10px)',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
   
 const ImageGallery = ({ value, hasImageGallery=false }) => {
@@ -54,130 +54,134 @@ const ImageGallery = ({ value, hasImageGallery=false }) => {
 
   if(hasImageGallery){ return (
     <>
-    <LightboxModal showModal={showLightbox}>
-    <span
-            onClick={()=>setShowLightbox(false)}>
-                X
-            </span>
-           <div
-           style={{
-           position: "relative",
-           width: "100%",
-           height: "100%",
-           maxWidth: "100%",
-            maxHeight: "100%",
+      <LightboxModal showModal={showLightbox}>
+        <span onClick={() => setShowLightbox(false)}>X</span>
+        <div
+          style={{
+            position: 'relative',
+            width: '100%',
+            height: '100%',
+            maxWidth: '100%',
+            maxHeight: '100%',
             margin: '0 0 1rem 0',
-            // paddingBottom: `min(350px, ${100 / (width / height)}%)` 
-           }}
-         >
-           <Image
-              className='rounded-lg'
-             src={urlBuilder({ projectId: 'k29n8cal', dataset: 'production' })
-               .image(value.images[selected])
-               .width(800)
-               .fit('max')
-               .auto('format')
-               .url()}
-             alt={selected.alt || ' '}
-             loading="lazy"
-             layout="fill" 
-             objectFit="contain"
-             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-             style={{
-               maxWidth: "100%",
-               maxHeight: "100%",
-               margin: '0 0 1rem 0',
-               // Display alongside text if image appears inside a block text span
-               display: 'block',
-               // Avoid jumping around with aspect-ratio CSS property
+            // paddingBottom: `min(350px, ${100 / (width / height)}%)`
+          }}
+        >
+          <Image
+            className="rounded-lg"
+            src={urlBuilder({ projectId: 'k29n8cal', dataset: 'production' })
+              .image(value.images[selected])
+              .width(800)
+              .fit('max')
+              .auto('format')
+              .url()}
+            alt={selected.alt || ' '}
+            loading="lazy"
+            layout="fill"
+            objectFit="contain"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            style={{
+              maxWidth: '100%',
+              maxHeight: '100%',
+              margin: '0 0 1rem 0',
+              // Display alongside text if image appears inside a block text span
+              display: 'block',
+              // Avoid jumping around with aspect-ratio CSS property
               // aspectRatio: width / height,
-             }}
-           />
-           </div>
-           {value.images[selected].alt !== undefined && value.images[selected].alt !== "" ? <span style={{fontSize: ".8rem", lineHeight: "1.5", maxWidth: "60ch"}}>{value.images[selected].alt}</span>:null}
-        <div style={{maxWidth: "800px"}}>
-    <FilmstripGrid>
-    {value.images !== undefined && value.images !== null ?
-         value.images.map((item,index)=> (
-         <div
-         key={`galleryImage${index}`}
-          style={{
-          position: "relative",
-          width: "80px",
-          height: "80px",
-          }}
-          onClick={()=>{
-            setSelected(index);
-       
-        }}
-        >
-          <Image
-             className='rounded-lg'
-            src={urlBuilder({ projectId: 'k29n8cal', dataset: 'production' })
-              .image(item)
-              .width(800)
-              .fit('max')
-              .auto('format')
-              .url()}
-            alt={value.alt || ' '}
-            loading="lazy"
-            layout="fill" 
-            objectFit="cover"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            style={{
-              maxWidth: "100%",
-              maxHeight: "100%",
-              margin: '0 0 1rem 0',
-              // Display alongside text if image appears inside a block text span
-            //  display: isInline ? 'inline-block' : 'block',
-              // Avoid jumping around with aspect-ratio CSS property
-             // aspectRatio: width / height,
             }}
           />
-          </div>)):null}
-    </FilmstripGrid>
-    </div>
-        </LightboxModal>
-    <FilmstripGrid>
-    {value.images !== undefined && value.images !== null ?
-         value.images.map((item,index)=> (
-         <div
-         key={`galleryImage${index}`}
-          style={{
-          position: "relative",
-          width: "120px",
-          height: "120px",
-          }}
-          onClick={()=>{
-            setSelected(index);
-            setShowLightbox(true);
-        }}
-        >
-          <Image
-             className='rounded-lg'
-            src={urlBuilder({ projectId: 'k29n8cal', dataset: 'production' })
-              .image(item)
-              .width(800)
-              .fit('max')
-              .auto('format')
-              .url()}
-            alt={value.alt || ' '}
-            loading="lazy"
-            layout="fill" 
-            objectFit="cover"
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            style={{
-              maxWidth: "100%",
-              maxHeight: "100%",
-              margin: '0 0 1rem 0',
-              // Display alongside text if image appears inside a block text span
-             // display: isInline ? 'inline-block' : 'block',
-              // Avoid jumping around with aspect-ratio CSS property
-            //  aspectRatio: width / height,
-            }}
-          />
-          </div>)):null}
-    </FilmstripGrid>
+        </div>
+        {value.images[selected].alt !== undefined && value.images[selected].alt !== '' ? (
+          <span style={{ fontSize: '.8rem', lineHeight: '1.5', maxWidth: '60ch' }}>
+            {value.images[selected].alt}
+          </span>
+        ) : null}
+        <div style={{ maxWidth: '800px' }}>
+          <FilmstripGrid>
+            {value.images !== undefined && value.images !== null
+              ? value.images.map((item, index) => (
+                  <div
+                    key={`galleryImage${index}`}
+                    style={{
+                      position: 'relative',
+                      width: '80px',
+                      height: '80px',
+                    }}
+                    onClick={() => {
+                      setSelected(index);
+                    }}
+                  >
+                    <Image
+                      className="rounded-lg"
+                      src={urlBuilder({ projectId: 'k29n8cal', dataset: 'production' })
+                        .image(item)
+                        .width(800)
+                        .fit('max')
+                        .auto('format')
+                        .url()}
+                      alt={value.alt || ' '}
+                      loading="lazy"
+                      layout="fill"
+                      objectFit="cover"
+                      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                      style={{
+                        maxWidth: '100%',
+                        maxHeight: '100%',
+                        margin: '0 0 1rem 0',
+                        // Display alongside text if image appears inside a block text span
+                        //  display: isInline ? 'inline-block' : 'block',
+                        // Avoid jumping around with aspect-ratio CSS property
+                        // aspectRatio: width / height,
+                      }}
+                    />
+                  </div>
+                ))
+              : null}
+          </FilmstripGrid>
+        </div>
+      </LightboxModal>
+      <FilmstripGrid>
+        {value.images !== undefined && value.images !== null
+          ? value.images.map((item, index) => (
+              <div
+                key={`galleryImage${index}`}
+                style={{
+                  position: 'relative',
+                  width: '120px',
+                  height: '120px',
+                }}
+                onClick={() => {
+                  setSelected(index);
+                  setShowLightbox(true);
+                }}
+              >
+                <Image
+                  className="rounded-lg"
+                  src={urlBuilder({ projectId: 'k29n8cal', dataset: 'production' })
+                    .image(item)
+                    .width(800)
+                    .fit('max')
+                    .auto('format')
+                    .url()}
+                  alt={value.alt || ' '}
+                  loading="lazy"
+                  layout="fill"
+                  objectFit="cover"
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                  style={{
+                    maxWidth: '100%',
+                    maxHeight: '100%',
+                    margin: '0 0 1rem 0',
+                    // Display alongside text if image appears inside a block text span
+                    // display: isInline ? 'inline-block' : 'block',
+                    // Avoid jumping around with aspect-ratio CSS property
+                    //  aspectRatio: width / height,
+                  }}
+                />
+              </div>
+            ))
+          : null}
+      </FilmstripGrid>
     </>
   );}
 };
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -133,7 +133,7 @@ const colorThemes = {
 };
 
 const components = {
-/* block: {
+  /* block: {
     h1: ({children}) => <h1 className='text-2xl leading-tight rounded-lg dark:bg-zinc-900 max-sm:text-center  sm:text-3xl' style={{marginTop: "100px !important"}}>{children}</h1>,
   },*/
   types: {
@@ -142,58 +142,60 @@ const components = {
 };
 
 const Project = ({ data }) => {
-  console.log(data)
+  console.log(data);
   return (
     <>
       <div className="flex items-center gap-6">
-        <Link
-          href={'/'}
-          className={`p-4 rounded-lg bg-zinc-900 ${
-            colorThemes[data.category.color ?? 'slate'].hoverBg
-          } transition-colors duration-500 ease-in-out h-full flex items-center`}
-        >
-          <svg viewBox="0 0 24 24" className="fill-none h-8 w-8">
-            <path
-              fill="currentColor"
-              d="M16.243 6.343L14.828 4.93 7.758 12l7.07 7.071 1.415-1.414L10.586 12l5.657-5.657z"
-            />
-          </svg>
-        </Link>
-        <div className="flex justify-between w-full items-center">
-          <div>
+        <div className="flex flex-col w-full items-center">
+          <div className="flex justify-between w-full">
+            <Link
+              href={'/'}
+              className={`p-3 rounded-lg bg-zinc-900 ${
+                colorThemes[data.category.color ?? 'slate'].hoverBg
+              } transition-colors duration-500 ease-in-out h-full flex items-center`}
+            >
+              <svg viewBox="0 0 24 24" className="fill-none h-8 w-8">
+                <path
+                  fill="currentColor"
+                  d="M16.243 6.343L14.828 4.93 7.758 12l7.07 7.071 1.415-1.414L10.586 12l5.657-5.657z"
+                />
+              </svg>
+            </Link>
+            {data?.externalLink?.enableLink ? (
+              <Link
+                href={data?.externalLink?.linkUrl}
+                target="_blank"
+                className={`px-6 py-3 rounded-lg bg-zinc-900 ${
+                  colorThemes[data.category.color ?? 'slate'].hoverBg
+                } transition-colors duration-500 ease-in-out h-full flex items-center`}
+              >
+                <div className="flex items-center gap-4 h-8">
+                  <span>{data?.externalLink?.linkName}</span>
+                  <span>
+                    <svg className="w-3 h-3" viewBox="0 0 13 13" fill="currentColor">
+                      <path d="M8.04998 6.36001L2.39398 12.018L0.97998 10.603L6.63698 4.94701L1.68698 -0.00299072H13V11.31L8.04998 6.36001Z" />
+                    </svg>
+                  </span>
+                </div>
+              </Link>
+            ) : null}
+          </div>
+          <div className="w-full mt-4">
             <h1
-              className={`text-6xl font-semibold ${
+              className={`text-4xl sm:text-5xl md:text-6xl font-semibold ${
                 colorThemes[data.category.color ?? 'slate'].text
               }`}
             >
               {data.title}
             </h1>
             <p
-              className={`text-xl font-semibold italic mt-4 ${
+              className={`text-lg md:text-xl font-semibold italic mt-2 md:mt-4 ${
                 colorThemes[data.category.color ?? 'slate'].text
               }`}
             >
               {data.tagLine}
             </p>
           </div>
-          {data?.externalLink?.enableLink ? (
-            <Link
-              href={data?.externalLink?.linkUrl}
-              target="_blank"
-              className={`px-6 py-3 rounded-lg bg-zinc-900 ${
-                colorThemes[data.category.color ?? 'slate'].hoverBg
-              } transition-colors duration-500 ease-in-out h-full flex items-center`}
-            >
-              <div className="flex items-center gap-4">
-                <span>{data?.externalLink?.linkName}</span>
-                <span>
-                  <svg className="w-3 h-3" viewBox="0 0 13 13" fill="currentColor">
-                    <path d="M8.04998 6.36001L2.39398 12.018L0.97998 10.603L6.63698 4.94701L1.68698 -0.00299072H13V11.31L8.04998 6.36001Z" />
-                  </svg>
-                </span>
-              </div>
-            </Link>
-          ) : null}
         </div>
       </div>
       <div
@@ -210,7 +212,6 @@ const Project = ({ data }) => {
       <ImageGallery value={data.imageGallery} hasImageGallery={data.imageGallery.isEnabled} 
       />
         <PortableText value={data.content} components={components} />
-      </div>
       </div>
     </>
   );
